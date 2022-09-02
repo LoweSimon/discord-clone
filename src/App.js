938d5606a922f079f019fd582b4d0d623635fc8e
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-09-02 15:36:40 +0100

    adding files to Git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Time to build a Discord Clone</h2>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
